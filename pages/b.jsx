dbe2042651d6945abbb47c623ce30a6b647a57e3
Nicolofranceschi <49746918+Nicolofranceschi@users.noreply.github.com>
2023-05-16 16:43:39 +0200

    nice

--- pages/b.jsx
+++ pages/b.jsx
@@ -695,12 +695,12 @@ export default function B() {
                           <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z" />
                         </svg>
                       </div>
-                      <p className="text-black font-bold ">Torna alla home</p>
+                      <p className="text-black font-bold ">Torna alla home per una nuova iscrizione o se hai finito</p>
                     </a>
                   </Link>
                 </>
               ) : (
-                <button className={ "w-full p-4 bg-sky-600 drop-shadow-2xl rounded-xl text-white font-bold"} type="submit" disabled={sendMail.isLoading} >
+                <button className={ "w-full p-4 bg-sky-600 disabled:animate-pulse drop-shadow-2xl rounded-xl text-white font-bold"} type="submit" disabled={sendMail.isLoading} >
                   {sendMail.isLoading ? "Caricamento, attendi l' invio dei dati rimanendo su questa pagina" : "INVIA"}
                 </button>
               )}
